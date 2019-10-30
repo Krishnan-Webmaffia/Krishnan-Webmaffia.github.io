3d83133738afb346a360693effca70173299b8ab
Krishnan-Webmaffia <56962938+Krishnan-Webmaffia@users.noreply.github.com>
2019-10-30 16:04:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -21,66 +21,62 @@ $(document).ready(function () {
         $(this).closest("li").find("p").toggle();
     });
 
-    // $("#faqaccordion").accordion({
-    //     collapsible: true
-    // });
+    $("#submitting").click(function () {
 
-    // $("#submitting").click(function () {
-
-    //     var fname = $("#fname").val();
-    //     var lname = $("#lname").val();
-    //     var email = $("#email").val();
-    //     var dob = $("#dob").val();
-    //     var mob = $("#mob").val();
-    //     var city = $("#city").val();
-    //     var state = $("#state").val();
-    //     var accept = $("#accept").val();
+        var fname = $("#fname").val();
+        var lname = $("#lname").val();
+        var email = $("#email").val();
+        var dob = $("#dob").val();
+        var mob = $("#mob").val();
+        var city = $("#city").val();
+        var state = $("#state").val();
+        var accept = $("#accept").val();
 
 
-    //     var re = /^[A-Za-z]+$/;
+        var re = /^[A-Za-z]+$/;
 
 
-    //     if (fname == "") {
-    //         alert("First Name cannot be empty. Try again");
-    //     } else if (lname == "") {
-    //         alert("Last Name cannot be empty. Try again");
-    //     } else if (email == "") {
-    //         alert("Email cannot be empty. Try again");
-    //     } else if (dob == "") {
-    //         alert("Date of Birth cannot be empty. Try again");
-    //     } else if (mob == "") {
-    //         alert("Mobile Number cannot be empty. Try again");
-    //     } else if (city == "") {
-    //         alert("City cannot be empty. Try again");
-    //     } else if (state == "") {
-    //         alert("State cannot be empty. Try again");
-    //     } else if (/\d/.test(fname)) {
-    //         alert("Invalid First Name. Try again");
-    //     } else if (/\d/.test(lname)) {
-    //         alert("Invalid Last Name. Try again");
-    //     } else if (/\d/.test(city)) {
-    //         alert("Invalid City. Try again");
-    //     } else if (/\d/.test(state)) {
-    //         alert("Invalid State. Try again");
-    //     } else if (!re.test(fname)) {
-    //         alert("Invalid First Name. Try again");
-    //     } else if (!re.test(lname)) {
-    //         alert("Invalid Last Name. Try again");
-    //     } else if (!re.test(city)) {
-    //         alert("Invalid City. Try again");
-    //     } else if (!re.test(state)) {
-    //         alert("Invalid State. Try again");
-    //     } else if (mob % 1 != 0) {
-    //         alert("Invalid Mobile Number. Try again");
-    //     } else if ($("#accept").prop("checked") == false) {
-    //         alert("Accept terms & conditions to continue.");
-    //     } else {
+        if (fname == "") {
+            alert("First Name cannot be empty. Try again");
+        } else if (lname == "") {
+            alert("Last Name cannot be empty. Try again");
+        } else if (email == "") {
+            alert("Email cannot be empty. Try again");
+        } else if (dob == "") {
+            alert("Date of Birth cannot be empty. Try again");
+        } else if (mob == "") {
+            alert("Mobile Number cannot be empty. Try again");
+        } else if (city == "") {
+            alert("City cannot be empty. Try again");
+        } else if (state == "") {
+            alert("State cannot be empty. Try again");
+        } else if (/\d/.test(fname)) {
+            alert("Invalid First Name. Try again");
+        } else if (/\d/.test(lname)) {
+            alert("Invalid Last Name. Try again");
+        } else if (/\d/.test(city)) {
+            alert("Invalid City. Try again");
+        } else if (/\d/.test(state)) {
+            alert("Invalid State. Try again");
+        } else if (!re.test(fname)) {
+            alert("Invalid First Name. Try again");
+        } else if (!re.test(lname)) {
+            alert("Invalid Last Name. Try again");
+        } else if (!re.test(city)) {
+            alert("Invalid City. Try again");
+        } else if (!re.test(state)) {
+            alert("Invalid State. Try again");
+        } else if (mob % 1 != 0) {
+            alert("Invalid Mobile Number. Try again");
+        } else if ($("#accept").prop("checked") == false) {
+            alert("Accept terms & conditions to continue.");
+        } else {
 
-    //         alert("Details saved: " + fname + " " + lname + " " + email + " " + dob + " " + mob + " " + city + " " + state);
+            alert("Details saved: " + fname + " " + lname + " " + email + " " + dob + " " + mob + " " + city + " " + state);
 
-    //     }
+        }
 
-    // });
+    });
 
 
 });
