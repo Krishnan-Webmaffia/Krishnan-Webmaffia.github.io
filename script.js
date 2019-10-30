dca6bda0eb5912b3dba181ff2e996889f404baef
Krishnan-Webmaffia <56962938+Krishnan-Webmaffia@users.noreply.github.com>
2019-10-30 18:58:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -30,51 +30,195 @@ $(document).ready(function () {
         var mob = $("#mob").val();
         var city = $("#city").val();
         var state = $("#state").val();
-        var accept = $("#accept").val();
 
+        var valid = {
+            fname : 0,
+            lname : 0,
+            email : 0,
+            dob : 0,
+            mob : 0,
+            city : 0,
+            state : 0,          
+        };
+
+        var flag= 0;
 
         var re = /^[A-Za-z]+$/;
 
 
-        if (fname == "") {
-            alert("First Name cannot be empty. Try again");
-        } else if (lname == "") {
-            alert("Last Name cannot be empty. Try again");
-        } else if (email == "") {
-            alert("Email cannot be empty. Try again");
-        } else if (dob == "") {
-            alert("Date of Birth cannot be empty. Try again");
-        } else if (mob == "") {
-            alert("Mobile Number cannot be empty. Try again");
-        } else if (city == "") {
-            alert("City cannot be empty. Try again");
-        } else if (state == "") {
-            alert("State cannot be empty. Try again");
-        } else if (/\d/.test(fname)) {
-            alert("Invalid First Name. Try again");
-        } else if (/\d/.test(lname)) {
-            alert("Invalid Last Name. Try again");
-        } else if (/\d/.test(city)) {
-            alert("Invalid City. Try again");
-        } else if (/\d/.test(state)) {
-            alert("Invalid State. Try again");
-        } else if (!re.test(fname)) {
-            alert("Invalid First Name. Try again");
-        } else if (!re.test(lname)) {
-            alert("Invalid Last Name. Try again");
-        } else if (!re.test(city)) {
-            alert("Invalid City. Try again");
-        } else if (!re.test(state)) {
-            alert("Invalid State. Try again");
-        } else if (mob % 1 != 0) {
-            alert("Invalid Mobile Number. Try again");
-        } else if ($("#accept").prop("checked") == false) {
-            alert("Accept terms & conditions to continue.");
+        if (fname === "") {
+            $("#fname").css("border-bottom", "1px solid red");
+            // alert("First Name cannot be empty. Try again");
+            valid.fname = 1;
         } else {
+            $("#fname").css("border-bottom", "1px solid black");
+            valid.fname = 0;
+        }
+        
+        if (lname === "") {
+            $("#lname").css("border-bottom", "1px solid red");
+            // alert("Last Name cannot be empty. Try again");
+            valid.lname = 1;
+        } else {
+            $("#lname").css("border-bottom", "1px solid black");
+            valid.lname = 0;
+        }
 
-            alert("Details saved: " + fname + " " + lname + " " + email + " " + dob + " " + mob + " " + city + " " + state);
+        if (email === "") {
+            $("#email").css("border-bottom", "1px solid red");
+            // alert("Email cannot be empty. Try again");
+            valid.email = 1;
+        } else {
+            $("#email").css("border-bottom", "1px solid black");
+            valid.email = 0;
+        }
+        
+        if (dob === "") {
+            $("#dob").css("border-bottom", "1px solid red");
+            // alert("Date of Birth cannot be empty. Try again");
+            valid.dob = 1;
+        } else {
+            $("#dob").css("border-bottom", "1px solid black");
+            valid.dob = 0;
+        }
+        
+        if (mob === "") {
+            $("#mob").css("border-bottom", "1px solid red");
+            // alert("Mobile Number cannot be empty. Try again");
+            valid.mob = 1;
+        } else {
+            $("#mob").css("border-bottom", "1px solid black");
+            valid.mob = 0;
+        }
+        
+        if (city === "") {
+            $("#city").css("border-bottom", "1px solid red");
+            // alert("City cannot be empty. Try again");
+            valid.city = 1;
+        } else {
+            $("#city").css("border-bottom", "1px solid black");
+            valid.city = 0;
+        }
+        
+        if (state === "") {
+            $("#state").css("border-bottom", "1px solid red");
+            // alert("State cannot be empty. Try again");
+            valid.state = 1;
+        } else {
+            $("#state").css("border-bottom", "1px solid black");
+            valid.state = 0;
+        }
+        
+        if (/\d/.test(fname)) {
+            $("#fname").css("border-bottom", "1px solid red");
+            // alert("Invalid First Name. Try again");
+            valid.fname = 1;
+        } else {
+            $("#fname").css("border-bottom", "1px solid black");
+            valid.fname = 0;
+        }
+        
+        if (/\d/.test(lname)) {
+            $("#lname").css("border-bottom", "1px solid red");
+            // alert("Invalid Last Name. Try again");
+            valid.lname = 0;
+        } else {
+            $("#lname").css("border-bottom", "1px solid black");
+            valid.lname = 0;
+        }
+        
+        if (/\d/.test(city)) {
+            $("#city").css("border-bottom", "1px solid red");
+            // alert("Invalid City. Try again");
+            valid.city = 1;
+        } else {
+            $("#city").css("border-bottom", "1px solid black");
+            valid.city = 0;
+        }
+        
+        if (/\d/.test(state)) {
+            $("#state").css("border-bottom", "1px solid red");
+            // alert("Invalid State. Try again");
+            valid.state = 1;
+        } else {
+            $("#state").css("border-bottom", "1px solid black");
+            valid.state = 0;
+        }
+        
+        if (!re.test(fname)) {
+            $("#fname").css("border-bottom", "1px solid red");
+            // alert("Invalid First Name. Try again");
+            valid.fname = 1;
+        } else {
+            $("#fname").css("border-bottom", "1px solid black");
+            valid.fname = 0;
+        }
+        
+        if (!re.test(lname)) {
+            $("#lname").css("border-bottom", "1px solid red");
+            // alert("Invalid Last Name. Try again");
+            valid.lname = 1;
+        } else {
+            $("#lname").css("border-bottom", "1px solid black");
+            valid.lname = 0;
+        }
+        
+        if (!re.test(city)) {
+            $("#city").css("border-bottom", "1px solid red");
+            // alert("Invalid City. Try again");
+            valid.city = 1;
+        } else {
+            $("#city").css("border-bottom", "1px solid black");
+            valid.city = 0;
+        }
+        
+        if (!re.test(state)) {
+            $("#state").css("border-bottom", "1px solid red");
+            // alert("Invalid State. Try again");
+            valid.state = 1;
+        } else {
+            $("#state").css("border-bottom", "1px solid black");
+            valid.state = 0;
+        }
+
+        if (mob!= "") {
+
+            if (mob % 1 != 0) {
+                $("#mob").css("border-bottom", "1px solid red");
+                // alert("Invalid Mobile Number. Try again");
+                valid.mob = 1;
+            } else {
+                $("#mob").css("border-bottom", "1px solid black");
+                valid.mob = 0;
+            }
 
         }
+
+        if (valid.fname === 0 && valid.lname === 0 && valid.email === 0 && valid.dob === 0 && valid.mob === 0 && valid.city === 0 && valid.state === 0) {
+
+            flag = 0;
+            
+        } else {
+
+            flag = 1;
+
+        }
+
+
+        
+        if(flag===0) {
+
+            if ($("#accept").prop("checked") == false) {
+                alert("Accept terms & conditions to continue.");
+            } else {
+
+                alert("Details saved: " + fname + " " + lname + " " + email + " " + dob + " " + mob + " " + city + " " + state);
+
+            }
+
+        }
+        
+        
 
     });
 
